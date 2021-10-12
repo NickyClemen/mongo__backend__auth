aee46a32870bf13d3ce9cad751a1d7223a2f8deb
Nicole Ordoqui Morales <40078599+NickyClemen@users.noreply.github.com>
2021-10-12 09:50:18 -0300

    Update UserProfile.service.ts

--- src/services/UserProfile.service.ts
+++ src/services/UserProfile.service.ts
@@ -24,12 +24,26 @@ export default class UserProfile {
 
         return UserProfile.instance;
     }
-
+    
+    /**
+    * Filtra el usuario en la base de datos por username.
+    * @async
+    * @method
+    * @param {String} username
+    * @returns {UserProfile} Objeto usuario.
+    */
     async findUserByUsername(username:string):Promise<IUserProfile|null> {
         return (await this.userProfile).findUserByUsername(username)
             .then((response:unknown) => response as IUserProfile);
     }
 
+    /**
+    * Filtra el usuario en la base de datos por email.
+    * @async
+    * @method
+    * @param {String} email
+    * @returns {UserProfile} Objeto usuario.
+    */
     async findUserByEmail(email:string):Promise<IUserProfile|null> {
         return (await this.userProfile).findUserByEmail(email)
             .then((response:unknown) => response as IUserProfile);
