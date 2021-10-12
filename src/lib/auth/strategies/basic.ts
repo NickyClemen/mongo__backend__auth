a368e0031e2c389307feb0edf9083382366bd80c
Nicole Ordoqui Morales <40078599+NickyClemen@users.noreply.github.com>
2021-10-12 09:51:06 -0300

    Update basic.ts

--- src/lib/auth/strategies/basic.ts
+++ src/lib/auth/strategies/basic.ts
@@ -13,7 +13,7 @@ passport.use(new BasicStrategy(async function(username:string, pass:string, cb:C
         const userProfile:IUserProfile|null = await userProfileService.findUserByUsername(username);
 
         if(!userProfile || userProfile === null) {
-            return cb(new Error('No existe el usuario en el sistema. Comuníquese con el administrador.'), false);
+            return cb(new Error('No existe el usuario en el sistema.'), false);
         }
 
         const { password }:IUserProfile = userProfile;
